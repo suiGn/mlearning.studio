d016eb7c15e6ca70e910065f88fb8dc5c1807714
Sui Gn <natsynapses@gmail.com>
2024-01-02 18:04:59 -0500

    express
    express

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
+app.use(cors());
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
